9b6bf40e6fce2dac215f80d6f175cf625f28a642
Martell <vovasamb@yandex.ru>
2022-05-27 22:15:40 +0300

    Хз, зачем этот файл

---
+++ app_test.py
@@ -0,0 +1,8 @@
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.get("http://127.0.0.1:5000/trending_searches/RU")
+    print(response.content.decode())
+    response = requests.get("http://127.0.0.1:5000/interest_over_time/US/Netflix")
+    print(response.content.decode())
